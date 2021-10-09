684ef402f3fdc186bacc11256fbfa645b7478485
5Hyeons <ohs2251@gmail.com>
2021-10-09 14:44:44 +0000

    chore: add parameter

--- efficientdet/model.py
+++ efficientdet/model.py
@@ -12,6 +12,9 @@ def get_net(checkpoint_path=None, box_weight=50, img_size=512):
     config.soft_nms = False
     config.max_det_per_image = 100
     config.box_loss_weight = box_weight
+    # config.label_smoothing = 0.2
+    # config.lagacy_focal = True
+    # config.jit_loss = True
 
     net = EfficientDet(config, pretrained_backbone=True)
     net.class_net = HeadNet(config, num_outputs=config.num_classes)
@@ -24,10 +27,10 @@ def get_net(checkpoint_path=None, box_weight=50, img_size=512):
     return net
 
 # Effdet config를 통해 모델 불러오기 + ckpt load
-def load_net(checkpoint_path, device):
+def load_net(checkpoint_path, device, img_size):
     config = get_efficientdet_config('tf_efficientdet_d5')
     config.num_classes = 10
-    config.image_size = (512,512)
+    config.image_size = (img_size, img_size)
     
     config.soft_nms = False
     config.max_det_per_image = 25
