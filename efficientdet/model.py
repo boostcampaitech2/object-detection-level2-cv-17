3ee09db442bdad0c11939f2507c960afdb8a579a
5Hyeons <ohs2251@gmail.com>
2021-10-10 12:10:31 +0000

    chore: load_net

--- efficientdet/model.py
+++ efficientdet/model.py
@@ -5,14 +5,14 @@ from effdet.efficientdet import HeadNet
 
 def get_net(checkpoint_path=None, box_weight=50, img_size=512):
     
-    config = get_efficientdet_config('tf_efficientdet_d5')
+    config = get_efficientdet_config('tf_efficientdet_d4')
     config.num_classes = 10
     config.image_size = (img_size, img_size)
     
     config.soft_nms = False
     config.max_det_per_image = 100
     config.box_loss_weight = box_weight
-    # config.label_smoothing = 0.2
+    config.label_smoothing = 0.2
     # config.lagacy_focal = True
     # config.jit_loss = True
     print(config)
@@ -33,8 +33,10 @@ def load_net(checkpoint_path, device, img_size):
     config.num_classes = 10
     config.image_size = (img_size, img_size)
     
-    config.soft_nms = False
-    config.max_det_per_image = 25
+    config.soft_nms = True
+    config.max_det_per_image = 100
+    config.label_smoothing = 0.2
+    
     
     net = EfficientDet(config, pretrained_backbone=False)
     net.class_net = HeadNet(config, num_outputs=config.num_classes)
