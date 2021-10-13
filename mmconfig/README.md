b426c1a331882db01339d518907e065b8c5fd616
w.sangbae <bsw0111@naver.com>
2021-10-13 08:35:35 +0000

    add : custom config in mmdetection

---
+++ mmconfig/README.md
@@ -0,0 +1,3 @@
+mmconfig 폴더를 mmdetection 폴더 안에 넣고 실행시켜야 합니다.
+
+실행법 : tools/train.py {config 경로} --seed {시드}
---
+++ mmconfig/test/config_fasterRCNN_2x.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../models/faster_rcnn_r50_fpn.py',
+    '../datasets/coco_detection_fasterRCNN.py',
+    '../schedules/schedule_2x.py', '../runtime/default_runtime.py'
+]
+
